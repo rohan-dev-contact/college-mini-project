3762c30d7d51455ef6ab9f77b991c2e077429556
rohan-dev-contact <123324163+rohan-dev-contact@users.noreply.github.com>
2024-03-24 15:13:21 +0530

    Update emailSender.php

--- views/emailSender.php
+++ views/emailSender.php
@@ -12,8 +12,8 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'rohan19mondal@gmail.com'; // Your Gmail email address
-    $mail->Password = 'ngmagrvmzllruskq';
+    $mail->Username = ''; // Your Gmail email address
+    $mail->Password = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
